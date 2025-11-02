80d511a24f7cd1e0653c564b3e8b8b622fc5fc1c
ImSatyamK <189542961+ImSatyamK@users.noreply.github.com>
2025-11-02 14:30:22 +0530

    Initial commit

---
+++ sys_exit.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python sys_exit.py <dataset_path> <epochs>")
+    sys.exit(1)
+
+print(sys.argv)
+
+# Run the commands below in terminal to understand
+
+# python sys_exit.py
+
+# python sys_exit.py randomArg
