61437879a487cc3142d1795b3987169601c07d77
ImSatyamK <189542961+ImSatyamK@users.noreply.github.com>
2025-11-02 14:15:28 +0530

    Initial commit

---
+++ print_args.py
@@ -0,0 +1,10 @@
+# print_args.py
+import sys
+
+print("All args:", sys.argv)
+
+# Run the commands below in terminal to see the passed arguments
+
+# python print_args.py
+
+# python print_args.py data.csv 50
